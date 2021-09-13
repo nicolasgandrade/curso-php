7f1873a2d5674ce419972927a79da18c38a4300d
Nicolas Guerrero <nicolas@pop-os.localdomain>
2021-09-13 17:51:07 -0300

    13/09 tarde

---
+++ 6_estruturas_de_controle/1_switch.php
@@ -0,0 +1,16 @@
+<?php
+
+    $x = 2;
+
+    switch($x) {
+        
+        case 0:
+            echo "X é igual a 0 <br>";
+            break;
+        case 1:
+            echo "X é igual a 1 <br>";
+            break;
+        default:
+            echo "X é diferente de 0 e 1";
+            
+    }
